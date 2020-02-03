d86ed13cd946758742fd2d55b6232b044088e11b
clinse <christoph.linse@prodyna.com>
2020-02-03 10:24:09 +0100

    removing rsa keys, adding aws token

---
+++ aws/java/src/aws_client.java
@@ -0,0 +1,32 @@
+/******************************************************************************
+ *  Compilation:  javac HelloWorld.java
+ *  Execution:    java HelloWorld
+ *
+ *  Prints "Hello, World". By tradition, this is everyone's first program.
+ *
+ *  % java HelloWorld
+ *  Hello, World
+ *
+ *  These 17 lines of text are comments. They are not part of the program;
+ *  they serve to remind us about its properties. The first two lines tell
+ *  us what to type to compile and test the program. The next line describes
+ *  the purpose of the program. The next few lines give a sample execution
+ *  of the program and the resulting output. We will always include such 
+ *  lines in our programs and encourage you to do the same.
+ *
+ ******************************************************************************/
+
+public class AmazonClient {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+    
+    String ACCESS_KEY_ID = "AKIA2E0A8F3B244C9986";
+    String SECRET_KEY = "7CE556A3BC234CC1FF9E8A5C324C0BB70AA21B6D";
+
+    AWSCredentials creds = new BasicAWSCredentials(ACCESS_KEY_ID, SECRET_KEY);
+    AmazonSimpleDBClient client = new AmazonSimpleDBClient(creds);
+}
+
